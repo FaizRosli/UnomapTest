827b79faade0c4d218cd73059fb9a117093023bd
FaizRosli <muhammadfaizrosli@yahoo.com>
2019-08-21 12:13:29 +0800

    fix delete user for admin.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -71,10 +71,8 @@ class AdminController extends Controller
     public function delete($id)
     {
         $user = \App\User::find($id);
-        $post = \App\Post::where('user_id',$id)->get();
         $user->delete();
-        $post->delete();
-    
+       
         return redirect('/Admin/index');
     }
 }
